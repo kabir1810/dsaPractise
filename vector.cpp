0a94fad53ceb10bc05627a820fcfa5c8d126aed4
kabir1810 <100670789+kabir1810@users.noreply.github.com>
2025-03-27 00:18:27 +0530

    vector

---
+++ vector.cpp
@@ -0,0 +1,23 @@
+#include<bitset>/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n,0);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int x=10;
+    arr.push_back(x);
+
+    sort(arr.begin(),arr.end());
+
+    int size=arr.size();
+
+
+
+    vector<int>v;
+    int x,y,z;
+    return 0;
+}
